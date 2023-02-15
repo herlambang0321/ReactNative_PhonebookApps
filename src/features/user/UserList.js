7f43660443f17a3c232e56cd3c4df3edb777d3f9
Herlambang Gunawan <herlambang88888888@gmail.com>
2023-02-15 12:07:29 +0700

    repair finish code done

--- src/features/user/UserList.js
+++ src/features/user/UserList.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { FlatList, View } from "react-native";
+import { FlatList, View, KeyboardAvoidingView, Platform } from "react-native";
 import { useSelector, useDispatch } from 'react-redux'
 import UserItem from "../../components/UserItem"
 
@@ -22,25 +22,31 @@ export default function UserList(props) {
     }, [dispatch]);
 
     return (
-        <View style={{ flex: 1 }}>
-            <FlatList
-                data={users}
-                initialNumToRender={7}
-                renderItem={({ item, index }) => (
-                    <UserItem
-                        key={item.id}
-                        no={index + 1}
-                        user={item}
-                        remove={() => dispatch(removeUserAsync(item.id))}
-                        resend={() => dispatch(addUserAsync({ id: item.id, name: item.name, phone: item.phone }))}
-                    />
-                )}
-                keyExtractor={(item) => item.id}
-                onEndReached={() => dispatch(loadmoreUser())}
-                onEndReachedThreshold={0.5}
-                style={{ maxHeight: 540 }}
-            />
-        </View>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS == "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform.OS == "ios" ? 0 : 20}
+            enabled={Platform.OS === "ios" ? true : false}>
+            <View style={{ flex: 1 }}>
+                <FlatList
+                    data={users}
+                    initialNumToRender={7}
+                    renderItem={({ item, index }) => (
+                        <UserItem
+                            key={item.id}
+                            no={index + 1}
+                            user={item}
+                            remove={() => dispatch(removeUserAsync(item.id))}
+                            resend={() => dispatch(addUserAsync({ id: item.id, name: item.name, phone: item.phone }))}
+                        />
+                    )}
+                    keyExtractor={(item) => item.id}
+                    onEndReached={() => dispatch(loadmoreUser())}
+                    onEndReachedThreshold={0.5}
+                    style={{ maxHeight: 540 }}
+                />
+            </View>
+        </KeyboardAvoidingView>
     )
 
 }
